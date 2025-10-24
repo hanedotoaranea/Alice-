a27e950fc0c03ca90c25001f4525ce5cde814c14
hanedotoaranea <arachnoideaqinki@gmail.com>
2025-10-25 00:52:14 +0500

    hanedotoaranea

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# lxqt-debian-13
+[![All releases](https://img.shields.io/github/v/release/hanedotoaranea/lxqt-debian-13?include_prereleases&style=for-the-badge&label=All+releases)](https://github.com/hanedotoaranea/lxqt-debian-13/releases)
+[![Repo size](https://img.shields.io/github/repo-size/hanedotoaranea/lxqt-debian-13?style=for-the-badge)](https://github.com/hanedotoaranea/lxqt-debian-13)
--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-lb config -d bullseye \
+lb config -d trixie \
           --debian-installer live \
-          --debian-installer-distribution bullseye \
+          --debian-installer-distribution trixie \
           --debian-installer-gui true \
           --archive-areas "main contrib non-free" \
-          --debootstrap-options "--variant=minbase" \
+          --debootstrap-options "--variant=minbase"
 
-          sudo lb build
+sudo lb build
