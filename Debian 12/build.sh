a13c8527a6f758ce58a3531ed8e2a7bd996671be
hanedotoaranea <arachnoideaqinki@gmail.com>
2025-12-08 13:41:06 +0500

    hane

---
+++ Debian 12/build.sh
@@ -0,0 +1,28 @@
+
+lb config -d bookworm \
+          --distribution bookworm 
+          --debian-installer live \
+          --debian-installer-distribution bookworm \
+          --debian-installer-gui true \
+          --distribution "bookworm" \
+          --architectures amd64 \
+          --binary-images iso-hybrid \
+          --iso-volume "Alice_$(date +%Y%m%d)" \  
+          --archive-areas "main contrib non-free non-free-firmware" \
+          --debootstrap-options "--variant=standard --include=sudo,locales,keyboard-configuration" \
+          --iso-publisher "hanedotoaranea - https://github.com/hanedotoaranea" \
+          --iso-application "GitHub:https://github.com/hanedotoaranea/" \
+          --checksums sha256 \
+          --mirror-bootstrap http://deb.debian.org/debian \
+          --mirror-binary http://deb.debian.org/debian \
+          --mirror-binary-security http://security.debian.org/debian-security \
+          --win32-loader false \
+          --apt-recommends true \
+          --security true \
+          --zsync true \
+          --updates true \              
+          --bootloader-timeout 5 \   
+          --hdd-label "ALICE_OS" \
+          --color \
+
+sudo lb build
