2605898fbd7d13b259798c0a7982ff880e04c603
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-08 23:32:41 +0500

    Update build.sh

--- Debian 12/build.sh
+++ Debian 12/build.sh
@@ -1,21 +1,8 @@
-lb config \
-  --distribution bookworm \
-  --architectures amd64 \
-  --binary-images iso-hybrid \
-  --mode debian-cd \
-  --iso-volume "Alice_Full_DVD_$(date +%Y%m%d)" \
-  --archive-areas "main contrib non-free non-free-firmware" \
-  --debian-installer true \
-  --debian-installer-gui false \
-  --mirror-bootstrap http://deb.debian.org/debian \
-  --mirror-binary http://deb.debian.org/debian \
-  --mirror-binary-security http://security.debian.org/debian-security \
-  --hdd-label "ALICE_OS" \
-  --bootloader syslinux \
-  --memtest none \
-  --win32-loader false \
-  --cache-packages true \
-  --apt-indices false \
-  --checksums sha256 \
-  
+  lb config -d bookworm \
+          --debian-installer live \
+          --debian-installer-distribution bookworm \
+          --debian-installer-gui true \
+          --iso-volume "Alice_Full_DVD_$(date +%Y%m%d)" \
+          --archive-areas "main contrib non-free" \
+          --debootstrap-options "--variant=minbase" \
 sudo lb build
