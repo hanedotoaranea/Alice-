ba6f6c010cfe908e47ca4417dd585c3b12771b0e
hanedotoaranea <153028549+hanedotoaranea@users.noreply.github.com>
2025-12-26 23:08:32 +0500

    Update build.sh

--- Debian 13/build.sh
+++ Debian 13/build.sh
@@ -1,4 +1,4 @@
- lb config -d bookworm \
+ lb config -d trixie \
           --debian-installer true \
           --debian-installer-gui true \
           --debootstrap-options "--variant=minbase" \
@@ -8,19 +8,18 @@
           --hdd-label "ALICE_OS" \
           --security true \
           --zsync true \
-          --updates true \   
+          --updates true \
           --iso-publisher "hanedotoaranea - https://github.com/hanedotoaranea" \
           --iso-application "GitHub:https://github.com/hanedotoaranea/" \
           --checksums sha256 \
           --mirror-bootstrap http://deb.debian.org/debian \
           --mirror-binary http://deb.debian.org/debian \
-          --mirror-binary-security http://security.debian.org/debian-security \
+          --mirror-binary-security http://security.debian.org/trixie-security \
           --win32-loader false \
-          --cache true \ 
+          --cache true \
           --binary-images iso-hybrid \
-          --firmware-binary true \ 
-          --cache-packages true \ 
-          --apt-recommends false \ 
-
+          --firmware-binary true \
+          --cache-packages true \
+          --apt-recommends false \
           
 sudo lb build
